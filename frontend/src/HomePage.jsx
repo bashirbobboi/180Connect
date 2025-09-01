c25a1afa8edad78b41dd5d1cba9a40211d547192
bashirbobboi <125030027+bashirbobboi@users.noreply.github.com>
2025-09-01 04:41:48 +0100

    fixed bug with import statement

--- frontend/src/HomePage.jsx
+++ frontend/src/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Layout } from "@/components/layout";
+import { Layout } from "@/components/Layout";
 import { useState, useEffect } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
